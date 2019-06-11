b6c1e5948bc9897b625cf42130ce77c8b79f4b55
22blackte <49483176+22blackte@users.noreply.github.com>
2019-06-11 08:24:52 -0400

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,19 @@
+def main()
+
+
+print("shift tab to start the program")
+
+print("what is the farenheit temperture")
+
+print("10*0")
+
+print("shift space to end program")
+
+input("celsius 0.62")
+
+print("calculate fahrenheit as (9/5)celsius = 32")
+print("fahrenhiet")
+
+print("celsius")
+
+print("logic")
